1464e3fd3f04201075b86dda4c61a7225d498fbf
dilunip <dilunip05@gmail.com>
2020-10-10 10:04:08 +0530

    Adding another Feature

--- sqcode.py
+++ sqcode.py
@@ -1,5 +1,10 @@
 number = int (raw_input ("Enter an integer number: "))
 
 square = number*number
+addition= number+number
+
 
 print "Square of {0} is {1} ".format (number, square)
+print "Addition of {0} with the same number is {1} ".format (number,addition )
+
+
